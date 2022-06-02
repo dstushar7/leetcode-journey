bffd2d266858be52abd9e0aaad99934a91eb8234
dstushar7 <tusharsarker97@gmail.com>
2022-06-03 01:47:27 +0600

    DS Practice

--- 217. Contains Duplicate.py
+++ 217. Contains Duplicate.py
@@ -2,10 +2,26 @@
 
 # https://leetcode.com/problems/contains-duplicate/
 
-nums = [1,2,3,4]
+nums = [1,2,3,1]
 
-for i in range(0,len(nums)-1):
-    for j in range(i+1,len(nums)):
-        if nums[i] == nums[j]:
-            print(True)
-print(False)
+# Complexity O(n^2) TLE error
+# for i in range(0,len(nums)-1):
+#     for j in range(i+1,len(nums)):
+#         if nums[i] == nums[j]:
+#             print(True)
+# print(False)
+
+
+# Complexity O(nlogn)
+# nums = sorted(nums)
+
+# for i in range(0,len(nums)-1):
+#     if nums[i] == nums[i+1]:
+#         print(True)
+# print(False)
+
+#Complexity O(n)
+if len(nums) == len(set(nums)):
+    print(False)
+else:
+    print(True)
