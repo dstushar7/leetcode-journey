1cb429f4d0a533b9c88439e1b43bc4d195c5304a
dstushar7 <tusharsarker97@gmail.com>
2022-06-04 22:57:55 +0600

    2nd day of #100daysofCode ends

---
+++ 876. Middle of the Linked List.py
@@ -0,0 +1,11 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
