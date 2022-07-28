9439d1d2ff6fd216d9990f80a66579afa0d6c02f
Dabobroto Sarkar <35169303+dstushar7@users.noreply.github.com>
2022-07-29 05:14:38 +0600

    Leetcode!!! Started Again!

--- 876. Middle of the Linked List.py
+++ 876. Middle of the Linked List.py
@@ -11,3 +11,10 @@
 
 # The problem is done in the leetcode editor
 
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow , fast = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
